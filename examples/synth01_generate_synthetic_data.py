f09b63a4049d9356d2150fd8c6f00859125accbe
toddkarin <toddkarin@lbl.gov>
2020-07-31 06:47:32 -0700

    Many modification to correspond to new updates

--- examples/synth01_generate_synthetic_data.py
+++ examples/synth01_generate_synthetic_data.py
@@ -9,7 +9,9 @@ import pickle
 import numpy as np
 import pandas as pd
 import pvlib
-
+import pvpro
+import time
+from pvpro.singlediode import calculate_temperature_coeffs
 
 # Load weather data
 data = np.load('123796_37.89_-122.26_search-point_37.876_-122.247.npz')
@@ -32,13 +34,17 @@ df.index = pd.to_datetime(df[['year', 'month', 'day', 'hour', 'minute']])
 # Clip shorter
 df = df[:'2002-01-01']
 
+# 15 minute interpolation
+df = df.resample('15T').interpolate('linear')
+
 # drop times when GHI is <= 10
-df.drop(df.index[df['ghi'] <= 10.], inplace=True)
+df.drop(df.index[df['ghi'] <= 1.], inplace=True)
 
 # assume poa = ghi, e.g., horizontal module
 df['poa_actual'] = df['ghi']
 # Simulate some noise on the measured poa irradiance.
-df['poa_meas'] = df['poa_actual'] + (np.random.random(df['ghi'].shape) - 0.5) * 2
+df['poa_meas'] = df['poa_actual'] + (
+        np.random.random(df['ghi'].shape) - 0.5) * 2
 
 # estimate module/cell temperature
 df['temperature_module_actual'] = pvlib.temperature.sapm_module(
@@ -61,14 +67,12 @@ df['temperature_cell_actual'] = pvlib.temperature.sapm_cell(
 df['temperature_module_meas'] = df['temperature_module_actual'] + (
         np.random.random(df['ghi'].shape) - 0.5) * 2
 
-
-
-
 q = 1.60218e-19  # Elementary charge in units of coulombs
 kb = 1.38066e-23  # Boltzmann's constant in units of J/K
 
 # time vector in years
-t_years = (df.index-df.index[0]).days/365
+t_years = (df.index - df.index[0]).days / 365
+
 
 def step_change(start_val, end_val, t_years, t_step):
     y = np.zeros_like(t_years) + start_val
@@ -76,85 +80,96 @@ def step_change(start_val, end_val, t_years, t_step):
             np.arctan(10 * (t_years - 2)) / np.pi + 0.5)
     return y
 
+
 # make up a parameter set for the De Soto model
 df['cells_in_series'] = 60
 df['alpha_sc'] = 0.001
 df['diode_factor'] = 1.15
-df['a_ref'] = df['diode_factor'] * df['cells_in_series'] * kb / q * (
+df['nNsVth_ref'] = df['diode_factor'] * df['cells_in_series'] * kb / q * (
         273.15 + 25)
-df['photocurrent_ref'] = 6.0 - 0.1 * t_years
+df['photocurrent_ref'] = 6.0 - 0 * 0.1 * t_years
 # df['photocurrent_ref'] = 6
 df['saturation_current_ref'] = 1e-9
-df['resistance_shunt_ref'] = step_change(1000, 100, t_years, 2)
-# df['resistance_shunt_ref'] = 1000
+# df['resistance_shunt_ref'] = step_change(1000, 100, t_years, 2)
+df['resistance_shunt_ref'] = 400
+df['conductance_shunt_extra'] = 0.000 + 0 * 0.0004 * t_years
+# df['resistance_shunt_ref'] = 1000 - 950/4*t_years
 # df.loc[t_years>2,'resistance_shunt_ref'] = 13
-df['resistance_series_ref'] = 0.5 + 0.02*t_years
+df['resistance_series_ref'] = 0.5 + 0 * 0 * 0.02 * t_years
 df['EgRef'] = 1.121
 df['dEgdT'] = -0.0002677
 
-# Calculate Desoto params
-iph, io, rs, rsh, nNsVth = pvlib.pvsystem.calcparams_desoto(
-    df['poa_actual'],
-    df['temperature_cell_actual'],
-    alpha_sc=df['alpha_sc'],
-    a_ref=df['a_ref'],
-    I_L_ref=df['photocurrent_ref'],
-    I_o_ref=df['saturation_current_ref'],
-    R_sh_ref=df['resistance_shunt_ref'],
-    R_s=df[
-        'resistance_series_ref'],
-    EgRef=df['EgRef'],
+out = pvpro.pvlib_single_diode(
+    effective_irradiance=df['poa_actual'],
+    temperature_cell=df['temperature_cell_actual'],
+    resistance_shunt_ref=df['resistance_shunt_ref'],
+    resistance_series_ref=df['resistance_series_ref'],
+    diode_factor=df['diode_factor'],
+    cells_in_series=df['cells_in_series'],
+    alpha_isc=df['alpha_sc'],
+    photocurrent_ref=df['photocurrent_ref'],
+    saturation_current_ref=df['saturation_current_ref'],
+    Eg_ref=df['EgRef'],
     dEgdT=df['dEgdT'],
+    conductance_shunt_extra=df['conductance_shunt_extra'],
+    method='lambertw',
+    ivcurve_pnts=None,
 )
 
+# column_renamer = {'v_oc': 'voc', 'i_mp': 'imp', 'v_mp': 'vmp', 'p_mp': 'pmp',
+#                   'i_sc': 'isc'}
+# out.rename(columns=column_renamer, inplace=True)
 
+for k in out.keys():
+    df[k] = out[k]
 
-df['photocurrent'] = iph
-df['saturation_current'] = io
-df['resistance_series'] = rs
-df['resistance_shunt'] = rsh
-df['nNsVth'] = nNsVth
-
-# Calcuate iv curve key points.
-ivcurves = pvlib.pvsystem.singlediode(iph, io, rs, rsh, nNsVth)
-
-# Do the same at reference conditions.
-iph_ref, io_ref, rs_ref, rsh_ref, nNsVth_ref = pvlib.pvsystem.calcparams_desoto(
+out_ref = pvpro.pvlib_single_diode(
     effective_irradiance=1000,
-    temp_cell=25,
-    alpha_sc=df['alpha_sc'],
-    a_ref=df['a_ref'],
-    I_L_ref=df['photocurrent_ref'],
-    I_o_ref=df['saturation_current_ref'],
-    R_sh_ref=df['resistance_shunt_ref'],
-    R_s=df[
-        'resistance_series_ref'],
-    EgRef=df['EgRef'],
+    temperature_cell=25,
+    resistance_shunt_ref=df['resistance_shunt_ref'],
+    resistance_series_ref=df['resistance_series_ref'],
+    diode_factor=df['diode_factor'],
+    cells_in_series=df['cells_in_series'],
+    alpha_isc=df['alpha_sc'],
+    photocurrent_ref=df['photocurrent_ref'],
+    saturation_current_ref=df['saturation_current_ref'],
+    Eg_ref=df['EgRef'],
     dEgdT=df['dEgdT'],
+    conductance_shunt_extra=df['conductance_shunt_extra'],
+    method='lambertw',
+    ivcurve_pnts=None,
 )
-ivcurves_ref = pvlib.pvsystem.singlediode(iph_ref, io_ref, rs_ref, rsh_ref, nNsVth_ref)
+# out_ref.rename(columns=column_renamer, inplace=True)
 
-# Add to dataframe.
-for k in ivcurves.keys():
-    df[k] = ivcurves[k]
+for k in out_ref.keys():
+    df[k + '_ref'] = out_ref[k]
 
+tempco = calculate_temperature_coeffs(
+    resistance_shunt_ref=df['resistance_shunt_ref'],
+    resistance_series_ref=df['resistance_series_ref'],
+    diode_factor=df['diode_factor'],
+    cells_in_series=df['cells_in_series'],
+    alpha_isc=df['alpha_sc'],
+    photocurrent_ref=df['photocurrent_ref'],
+    saturation_current_ref=df['saturation_current_ref'],
+    band_gap_ref=df['EgRef'],
+    dEgdT=df['dEgdT'],
+    conductance_shunt_extra=df['conductance_shunt_extra'],
+)
+
+for k in tempco:
+    df[k] = tempco[k]
 
 # Set operation point at v_mp/i_mp except at low irradiance.
-df['v_operation'] = df['v_mp']
-df['i_operation'] = df['i_mp']
+df['v_operation'] = df['vmp']
+df['i_operation'] = df['imp']
 # Set Voc points at low irradiances.
-df.loc[df['poa_actual'] < 50, 'v_operation'] = df.loc[df['poa_actual'] < 50, 'v_oc']
+df.loc[df['poa_actual'] < 50, 'v_operation'] = df.loc[
+    df['poa_actual'] < 50, 'voc']
 df.loc[df['poa_actual'] < 50, 'i_operation'] = 0
 
-
-# Add reference points to dataframe.
-for k in ivcurves_ref.keys():
-    df[k + '_ref'] = ivcurves_ref[k]
-
-
-
 df.to_pickle('synth01_out.pkl')
-
+print('done!')
 
 # with open('synth_data.dat', 'wb') as outfile:
 #     pickle.dump([module_parameters, supplement, ivcurves], outfile)
