86aecc9cdfc64626d40a01d30544cabe2f12136f
toddkarin <toddkarin@lbl.gov>
2021-01-21 07:19:23 -0800

    Clean up, change to NIST data

--- examples/example_estimate_number_series_parallel.py
+++ examples/example_estimate_number_series_parallel.py
@@ -1,9 +1,15 @@
 """
 
 This is a simple and fast demonstration for estimating the number of modules
-in series and parallel in a power block.
+in series and parallel in a power block. First, estimate imp_ref and vmp_ref,
+then divide by datahseet values to get estimated number of modules in series
+and parallel.
 
-Run synth01_generate_synthetic_data.py first to generate a sample data file.
+Data is taken from the NIST ground array [1], resampled to 15 minutes.
+
+[1] Matthew T. Boyd. High-Speed Monitoring of Multiple Grid-Connected
+Photovoltaic Array Configurations. NIST Technical Note 1896. [2015]
+https://doi.org/10.6028/NIST.TN.1896
 
 @author: toddkarin
 """
@@ -12,62 +18,73 @@ import numpy as np
 import pandas as pd
 
 import matplotlib
+
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
-from pvpro.fit import production_data_curve_fit
 from pvpro.classify import classify_operating_mode
 from pvpro.estimate import estimate_imp_ref, estimate_vmp_ref
 from pvlib.temperature import sapm_cell_from_module
 
-# Import synthetic data for a single module.
-df = pd.read_pickle('synth01_out.pkl')
+# Import NIST ground array test data
+df = pd.read_csv('nist_ground_resampled-15-min.csv')
 
-# Clip dataframe shorter.
-df = df[:5000]
+# Keys of interest in data frame
+current_key = 'InvIDCin_Avg'
+voltage_key = 'InvVDCin_Avg'
+temperature_module_key = 'RTD_C_Avg_4'
+poa_key = 'RefCell1_Wm2_Avg'
 
-# Multiply by a known number of modules in parallel and series
-parallel_strings = 5
-modules_in_series = 22
-df['v_dc'] = df['v_dc'] * parallel_strings
-df['i_dc'] = df['i_dc'] * modules_in_series
+# Clip dataframe to avoid bad data on startup. Could improve fit using
+# better filtering of bad data
+df = df[np.logical_and.reduce((
+    df[temperature_module_key] < 50,
+    df[temperature_module_key] > 15,
+    df.index > df.index[20000],
+    df.index < df.index[25000],
+))
+]
 
-# Datasheet values
-imp_ref_datasheet = df['i_mp_ref'][0]
-vmp_ref_datasheet = df['v_mp_ref'][0]
+# Datasheet values (Sharp NU-U235F2)'
+imp_ref_datasheet = 7.84
+vmp_ref_datasheet = 30
 
 # Estimate cell temperature.
-df['temperature_cell_meas'] = sapm_cell_from_module(
-    module_temperature=df['temperature_module_meas'],
-    poa_global=df['poa_meas'],
+df['temperature_cell'] = sapm_cell_from_module(
+    module_temperature=df[temperature_module_key],
+    poa_global=df[poa_key],
     deltaT=3)
 
-
-# Classify operating modes, this is a simple fast algorithm to find MPP.
-df['operating_cls'] = classify_operating_mode(voltage=df['v_dc'],
-                                              current=df['i_dc'],
+# Classify operating modes, this is a simple fast algorithm to find MPP points.
+df['operating_cls'] = classify_operating_mode(voltage=df[voltage_key],
+                                              current=df[current_key],
                                               power_clip=np.inf)
 # Mask off MPP points
 mpp = df['operating_cls'] == 0
 
 # Estimate imp ref
-est_imp = estimate_imp_ref(poa=df.loc[mpp, 'poa_meas'],
-                           temperature_cell=df.loc[
-                               mpp, 'temperature_cell_meas'],
-                           imp=df.loc[mpp, 'i_dc'],
+est_imp = estimate_imp_ref(poa=df.loc[mpp, poa_key],
+                           temperature_cell=df.loc[mpp, 'temperature_cell'],
+                           imp=df.loc[mpp, current_key],
                            figure=True
                            )
-print('Imp ref estimate: {}'.format(est_imp['i_mp_ref']))
-
-# Estimate parallel strings
-print('Parallel strings: {}'.format(est_imp['i_mp_ref'] / imp_ref_datasheet))
-
 # Estimate Vmp ref
-est_vmp = estimate_vmp_ref(poa=df.loc[mpp, 'poa_meas'],
-                           temperature_cell=df.loc[
-                               mpp, 'temperature_cell_meas'],
-                           vmp=df.loc[mpp, 'v_dc'],
+est_vmp = estimate_vmp_ref(poa=df.loc[mpp, poa_key],
+                           temperature_cell=df.loc[mpp, temperature_module_key],
+                           vmp=df.loc[mpp, voltage_key],
                            figure=True,
                            )
-print('Vmp ref estimate: {}'.format(est_vmp['v_mp_ref']))
-print('Modules in series: {}'.format(est_vmp['v_mp_ref'] / vmp_ref_datasheet))
+
+# Compare estimated values and ground truth
+info = {
+    'Imp_ref power block estimate': est_imp['i_mp_ref'],
+    'Imp_ref datasheet': imp_ref_datasheet,
+    'parallel_strings estimate': est_imp['i_mp_ref'] / imp_ref_datasheet,
+    'parallel_strings true': 96,
+    'Vmp_ref power block estimate': est_vmp['v_mp_ref'],
+    'Vmp_ref datasheet': vmp_ref_datasheet,
+    'modules_per_string estimate': est_vmp['v_mp_ref'] / vmp_ref_datasheet,
+    'modules_per_string true': 12,
+}
+
+print(pd.Series(info))
