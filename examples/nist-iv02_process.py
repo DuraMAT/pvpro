d312ceacac2ac7a230101dbbd5a98f6c30d744cf
toddkarin <toddkarin@lbl.gov>
2021-05-13 13:53:06 -0700

    document

--- examples/nist-iv02_process.py
+++ examples/nist-iv02_process.py
@@ -1,3 +1,7 @@
+"""
+Example process of ivcurves from NIST dataset.
+"""
+
 import os
 import pandas as pd
 import numpy as np
@@ -7,20 +11,18 @@ from pvlib.temperature import sapm_cell_from_module
 import matplotlib
 import matplotlib.pyplot as plt
 
+# Used simple algorithm to extract v_oc, v_mp, i_mp, i_sc. Can overwrite with
+# better extraction method if necessary.
 
-
-ivcurves = pd.read_pickle(os.path.join('data','nist_iv_curve_sample.pkl'))
-
+ivcurves = pd.read_pickle(os.path.join('data', 'nist_iv_curve_sample.pkl'))
 
 plt.figure(0)
 plt.clf()
 k = 20
-plt.plot(ivcurves.loc[k,'v'],ivcurves.loc[k,'i'])
-
+plt.plot(ivcurves.loc[k, 'v'], ivcurves.loc[k, 'i'])
 
 specs = {'cells_in_series': 60,
-        'alpha_sc': 0.053*1e-2*8.6,
-        'beta_voc': -0.351*1e-2*37.0}
+         'alpha_sc': 0.053 * 1e-2 * 8.6,
+         'beta_voc': -0.351 * 1e-2 * 37.0}
 
-desoto = fit_desoto_sandia(ivcurves,specs)
-
+desoto = fit_desoto_sandia(ivcurves, specs)
