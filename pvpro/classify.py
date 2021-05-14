fa97d49f4dbe981dba374d820f60e4cba328351c
toddkarin <toddkarin@lbl.gov>
2021-05-14 11:03:13 -0700

    Adding build_operating_cls

--- pvpro/classify.py
+++ pvpro/classify.py
@@ -1,14 +1,18 @@
 import numpy as np
+from pvanalytics.features import clipping
+import pandas as pd
 
 def classify_operating_mode(voltage, current,
                             power_clip=np.inf,
-                            method='fraction'):
+                            method='fraction',
+                            clipped_times=None,
+                            freq='15min'):
     """
 
     Parameters
     ----------
     voltage
-    current
+    currente
     method
 
     Returns
@@ -23,7 +27,6 @@ def classify_operating_mode(voltage, current,
         -1: No power/inverter off
         -2: Other
     """
-
 
     cls = np.zeros(np.shape(voltage)) - 1
 
@@ -40,8 +43,40 @@ def classify_operating_mode(voltage, current,
     cls[np.logical_and(current < current.max() * 0.01,
                        voltage > voltage.max() * 0.01)] = 1
 
-    # Clipped data. Easy algorithm.
-    cls[current * voltage > power_clip] = 2
+    if clipped_times is None:
+        clipped_times = clipping.geometric(
+            ac_power=voltage * current,
+            freq=freq)
+
+    # Clipped data.
+    cls[clipped_times] = 2
 
     return cls
 
+
+def build_operating_cls(df):
+    if isinstance(df, pd.DataFrame):
+        operating_cls = np.zeros(len(df), dtype='int')
+    else:
+        operating_cls = np.zeros(df['high_V'].shape,dtype='int')
+
+    # df.loc[:, 'operating_cls'] = 0
+    operating_cls[np.logical_and(
+        np.logical_not(df['high_v']),
+        np.logical_not(df['daytime']))] = -1
+
+    operating_cls[
+        np.logical_and(
+            df['high_v'],
+            np.logical_or(np.logical_not(df['daytime']), df['low_p'])
+        )] = 1
+
+    operating_cls[df['clipped_times']] = 2
+
+    operating_cls[
+        np.logical_or(
+            df['missing_data'],
+            np.logical_not(df['no_errors'])
+        )] = -2
+
+    return operating_cls
