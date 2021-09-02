41777ec829d7f6ab1ff1a3192d958e6d5cd3b0e7
toddkarin <toddkarin@lbl.gov>
2021-09-02 09:19:31 -0700

    Add calculate_rms_error function

--- pvpro/postprocess.py
+++ pvpro/postprocess.py
@@ -28,3 +28,35 @@ def analyze_yoy(pfit):
                 'median': np.median(pfit[k])}
 
     return out
+
+
+def calculate_rms_error(pfit, df,zero_mean=False):
+    dft = pd.DataFrame()
+
+    keys = ['diode_factor',
+            'photocurrent_ref', 'saturation_current_ref',
+            'resistance_series_ref',
+            'conductance_shunt_extra', 'resistance_shunt_ref',
+            'nNsVth_ref', 'i_sc_ref', 'v_oc_ref',
+            'i_mp_ref', 'v_mp_ref', 'p_mp_ref']
+
+    for k in range(len(pfit)):
+        cax = np.logical_and(df.index >= pfit['t_start'].iloc[k],
+                             df.index < pfit['t_end'].iloc[k])
+        dfcurr_mean = df[cax][keys].mean()
+
+        for key in dfcurr_mean.keys():
+            dft.loc[pfit['t_start'].iloc[k], key] = dfcurr_mean[key]
+
+
+    rms_error = {}
+    for k in keys:
+        if zero_mean:
+            p1 = dft[k] - np.median(dft[k])
+            p2 = pfit[k] - np.median(pfit[k])
+        else:
+            p1 = dft[k]
+            p2 = pfit[k]
+        rms_error[k] = np.sqrt(np.mean((p1-p2) ** 2))
+
+    return rms_error
