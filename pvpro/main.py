68eb3991059f4106e4f7f2d057ed85da4ac8b676
Baojie <lbjxx@126.com>
2022-10-11 12:37:42 -0700

    update functions

--- pvpro/main.py
+++ pvpro/main.py
@@ -6,28 +6,28 @@ import pandas as pd
 
 import datetime
 import os
-import warnings
 import time
-from tqdm import tqdm
-
+import scipy
+import shutil
+import seaborn as sns
 import matplotlib.pyplot as plt
-from pvpro.singlediode import estimate_Eg_dEgdT
+
+from scipy.optimize import minimize
+from tqdm import tqdm
+from functools import partial
+
+
+from matplotlib.colors import LinearSegmentedColormap
 
 from solardatatools import DataHandler
 from pvlib.temperature import sapm_cell_from_module
 
+from pvpro.singlediode import estimate_Eg_dEgdT
 from pvpro.singlediode import pv_system_single_diode_model
-from pvpro.fit import production_data_curve_fit
+from pvpro.estimate import estimate_singlediode_params
 
-from pvpro.estimate import estimate_singlediode_params, estimate_imp_ref, \
-    estimate_vmp_ref
-
-import shutil
 from pvanalytics.features import clipping
-
-from matplotlib.colors import LinearSegmentedColormap
-import seaborn as sns
-
+from sklearn.linear_model import LinearRegression
 
 class PvProHandler:
     """
@@ -428,7 +428,7 @@ class PvProHandler:
                     print('current for fit: ',
                           df[self.current_key] / self.parallel_strings)
 
-                fit_result_iter = production_data_curve_fit(
+                fit_result_iter = self.production_data_curve_fit(
                     temperature_cell=np.array(df['temperature_cell']),
                     effective_irradiance=np.array(df[self.irradiance_poa_key]),
                     operating_cls=np.array(df['operating_cls']),
@@ -588,6 +588,469 @@ class PvProHandler:
 
         print(
             'Elapsed time: {:.2f} min'.format((time.time() - start_time) / 60))
+
+    def _x_to_p(self, x : array, key : str):
+        """
+        Change from numerical fit value (x) to physical parameter (p). This
+        transformation improves the numerical performance of the fitting algorithm.
+
+        Parameters
+        ----------
+        x : ndarray
+
+            Value of parameter for numerical fitting.
+
+        key : str
+
+            parameter to change, can be 'diode_factor', 'photocurrent_ref',
+            'saturation_current_ref', 'resistance_series_ref',
+            'resistance_shunt_ref', 'conductance_shunt_extra'
+
+
+        Returns
+        -------
+        p : ndarray
+
+            Value of physical parameter.
+
+
+        """
+        if key == 'saturation_current_ref':
+            return np.exp(x-23)
+        
+        elif key == 'saturation_current':
+            return np.exp(x-23)
+        elif key == 'resistance_shunt_ref':
+            return np.exp(2 * (x - 1))
+        elif key == 'resistance_shunt':
+            return np.exp(2 * (x - 1))
+        elif key == 'alpha_isc':
+            return x * 1e-3
+        elif key == 'resistance_series_ref':
+            return x/2.2
+        elif key == 'resistance_series':
+            return x/2.2
+        else:
+            return x
+
+
+    def _p_to_x(self, p : array, key : str):
+        """
+        Change from physical parameter (p) to numerical fit value (x). This
+        transformation improves the numerical performance of the fitting algorithm.
+
+        Parameters
+        ----------
+        p : ndarray
+
+            Value of physical parameter.
+
+        key : str
+
+            parameter to change, can be 'diode_factor', 'photocurrent_ref',
+            'saturation_current_ref', 'resistance_series_ref',
+            'resistance_shunt_ref', 'conductance_shunt_extra'
+
+        Returns
+        -------
+        x : ndarray
+
+            Value of parameter for numerical fitting.
+        """
+        if key == 'saturation_current_ref':
+            return np.log(p) + 23
+        elif key == 'resistance_shunt_ref':
+            return np.log(p) / 2 + 1
+        elif key == 'resistance_shunt':
+            return np.log(p) / 2 + 1
+        elif key == 'resistance_series_ref':
+            return p*2.2
+        elif key == 'resistance_series':
+            return p*2.2
+        elif key == 'saturation_current':
+            return np.log(p)+23
+        elif key == 'alpha_isc':
+            return p * 1e3
+        else:
+            return p
+
+
+    def _pvpro_L2_loss(self, x : array, sdm : 'function', voltage : array, current : array, voltage_scale : float, current_scale : float,
+                    weights : float, fit_params : list):
+        voltage_fit, current_fit = sdm(
+            **{param: self._x_to_p(x[n], param) for n, param in
+            zip(range(len(x)), fit_params)}
+        )
+
+        # Note that summing first and then calling nanmean is slightly faster.
+        return np.nanmean(((voltage_fit - voltage) * weights / voltage_scale) ** 2 + \
+                        ((current_fit - current) * weights / current_scale) ** 2)
+
+    def production_data_curve_fit(self,
+        temperature_cell : array,
+        effective_irradiance : array,
+        operating_cls : array,
+        voltage : array,
+        current : array,
+        cells_in_series : int =60,
+        band_gap_ref : float = None,
+        dEgdT : float = None,
+        p0 : dict =None,
+        lower_bounds : float =None,
+        upper_bounds : float =None,
+        alpha_isc : float =None,
+        diode_factor : array =None,
+        photocurrent_ref : array =None,
+        saturation_current_ref : array =None,
+        resistance_series_ref : array =None,
+        resistance_shunt_ref : array =None,
+        conductance_shunt_extra : array =None,
+        verbose : bool =False,
+        solver : str ='L-BFGS-B',
+        singlediode_method : str ='fast',
+        method : str ='minimize',
+        use_mpp_points : bool =True,
+        use_voc_points : bool =True,
+        use_clip_points : bool =True,
+        # fit_params=None,
+        saturation_current_multistart : array =None,
+        brute_number_grid_points : int =2
+        ):
+        """
+        Use curve fitting to find best-fit single-diode-model paramters given the
+        operating data.
+
+        Data to be fit is supplied in 1xN vectors as the inputs temperature_cell,
+        effective_irradiance, operation_cls, voltage and current. Each of these
+        inputs must have the same length.
+
+        The inputs lower_bound, upper_bound, and p0 are all dictionaries
+        with the same keys.
+
+        Parameters
+        ----------
+        temperature_cell : ndarray
+
+            Temperature of PV cell in C.
+
+        effective_irradiance : ndarray
+
+            Effective irradiance reaching module in W/m2.
+
+        operating_cls : ndarray
+
+            Integer describing the type of operating point. See
+            classify_operating_mode for a description of the operating points.
+
+        voltage : ndarray
+
+            DC voltage of the PV module. To get from a string voltage to the
+            effective module voltage, divide by the string size.
+
+        current : ndarray
+
+            DC current of the PV module. To get from an array current to the
+            effective module current, divide by the number of strings in parallel.
+
+        lower_bounds : dict
+
+            dictionary of the lower bound for each fit parameter.
+
+        upper_bounds : dict
+
+            dictionary of the upper bound for each fit parameter.
+
+        alpha_isc : numeric
+
+            Temperature coefficient of short circuit current in A/C. If not
+            provided then alpha_isc is a fit paramaeter. Typically it is not
+            suggested to make alpha_isc a fit parameter since it only slightly
+            affects module operating voltage/current.
+
+        diode_factor : numeric or None (default).
+
+            diode ideality factor. If not provided, then diode_factor is a fit
+            parameter.
+
+        photocurrent_ref : numeric or None (default)
+
+            Reference photocurrent in A. If not provided, then photocurrent_ref
+            is a fit parameter.
+
+        saturation_current_ref : numeric or None (default)
+
+            Reference saturation current in A. If not provided or if set to None,
+            then saturation_current_ref is a fit parameter.
+
+        resistance_series_ref : numeric or None (default)
+
+            Reference series resistance in Ohms. If not provided or if set to None,
+            then resistance_series_ref is a fit parameter.
+
+        resistance_shunt_ref : numeric or None (default)
+
+            Reference shunt resistance in Ohms. If not provided or if set to None,
+            then resistance_shunt_ref is a fit parameter.
+
+        p0 : dict
+
+            Dictionary providing startpoint.
+
+        cells_in_series : int
+
+            Number of cells in series in the module.
+
+        band_gap_ref : float
+
+            Band gap at reference conditions in eV.
+
+        verbose : bool
+
+            Display fit output.
+
+        Returns
+        -------
+
+        """
+        voltage = np.array(voltage)
+        current = np.array(current)
+        effective_irradiance = np.array(effective_irradiance)
+        temperature_cell = np.array(temperature_cell)
+        operating_cls = np.array(operating_cls)
+
+        # Check all the same length:
+        if len(np.unique([len(voltage), len(current), len(effective_irradiance),
+                        len(temperature_cell), len(operating_cls)])) > 1:
+            raise Exception("Length of inputs 'voltage', 'current', 'effective_irradiance', 'temperature_cell', 'operating_cls' must all be the same." )
+
+        if p0 is None:
+            p0 = dict(
+                diode_factor=1.0,
+                photocurrent_ref=6,
+                saturation_current_ref=1e-9,
+                resistance_series_ref=0.2,
+                conductance_shunt_extra=0.001,
+                alpha_isc=0.002
+            )
+
+        # if fit_params is None:
+
+        fit_params = []
+        if diode_factor is None:
+            fit_params.append('diode_factor')
+        if photocurrent_ref is None:
+            fit_params.append('photocurrent_ref')
+        if saturation_current_ref is None:
+            fit_params.append('saturation_current_ref')
+        if resistance_series_ref is None:
+            fit_params.append('resistance_series_ref')
+        if conductance_shunt_extra is None:
+            fit_params.append('conductance_shunt_extra')
+        if resistance_shunt_ref is None:
+            fit_params.append('resistance_shunt_ref')
+        if alpha_isc is None:
+            fit_params.append('alpha_isc')
+        if band_gap_ref is None:
+            fit_params.append('band_gap_ref')
+
+        if lower_bounds is None:
+            lower_bounds = dict(
+                diode_factor=0.5,
+                photocurrent_ref=0.01,
+                saturation_current_ref=1e-13,
+                resistance_series_ref=0,
+                conductance_shunt_extra=0,
+                alpha_isc=0,
+                band_gap_ref=0.5,
+            )
+
+        if upper_bounds is None:
+            upper_bounds = dict(
+                diode_factor=2,
+                photocurrent_ref=20,
+                saturation_current_ref=1e-5,
+                resistance_series_ref=1,
+                conductance_shunt_extra=10,
+                alpha_isc=1,
+                band_gap_ref=2.0,
+            )
+
+        if saturation_current_multistart is None:
+            saturation_current_multistart = [0.2, 0.5, 1, 2, 5]
+
+        # Only keep points belonging to certain operating classes
+        cls_keepers = np.logical_or.reduce(
+            (use_mpp_points * (operating_cls == 0),
+            use_voc_points * (operating_cls == 1),
+            use_clip_points * (operating_cls == 2),
+            ))
+
+        keepers = np.logical_and.reduce((cls_keepers,
+                                        np.isfinite(voltage),
+                                        np.isfinite(current),
+                                        np.isfinite(effective_irradiance),
+                                        np.isfinite(temperature_cell)
+                                        ))
+        effective_irradiance = effective_irradiance[keepers]
+        temperature_cell = temperature_cell[keepers]
+        operating_cls = operating_cls[keepers]
+        voltage = voltage[keepers]
+        current = current[keepers]
+
+        weights = np.zeros_like(operating_cls)
+        weights[operating_cls == 0] = 1
+        weights[operating_cls == 1] = 1
+        weights[operating_cls == 2] = 1
+
+        if verbose:
+            print('Total number points: {}'.format(len(operating_cls)))
+            print('Number MPP points: {}'.format(np.sum(operating_cls == 0)))
+            print('Number Voc points: {}'.format(np.sum(operating_cls == 1)))
+            print('Number Clipped points: {}'.format(np.sum(operating_cls == 2)))
+
+        fixed_params = {}
+        # Set kwargs
+        if not diode_factor == None:
+            fixed_params['diode_factor'] = diode_factor
+        if not photocurrent_ref == None:
+            fixed_params['photocurrent_ref'] = photocurrent_ref
+        if not saturation_current_ref == None:
+            fixed_params['saturation_current_ref'] = saturation_current_ref
+        if not resistance_series_ref == None:
+            fixed_params['resistance_series_ref'] = resistance_series_ref
+        if not resistance_shunt_ref == None:
+            fixed_params['resistance_shunt_ref'] = resistance_shunt_ref
+        if not alpha_isc == None:
+            fixed_params['alpha_isc'] = alpha_isc
+        if not conductance_shunt_extra == None:
+            fixed_params['conductance_shunt_extra'] = conductance_shunt_extra
+        if not band_gap_ref == None:
+            fixed_params['band_gap_ref'] = band_gap_ref
+        if not dEgdT == None:
+            fixed_params['dEgdT'] = dEgdT
+
+
+        # If no points left after removing unused classes, function returns.
+        if len(effective_irradiance) == 0 or len(
+                operating_cls) == 0 or len(voltage) == 0 or len(current) == 0:
+
+            if verbose:
+                print('No valid values received.')
+
+            return {'p': {key: np.nan for key in fit_params},
+                    'fixed_params': fixed_params,
+                    'residual': np.nan,
+                    'p0': p0,
+                    }
+
+        model_kwargs = dict(
+            effective_irradiance=effective_irradiance,
+            temperature_cell=temperature_cell,
+            operating_cls=operating_cls,
+            cells_in_series=cells_in_series,
+            voltage_operation=voltage,
+            current_operation=current,
+            singlediode_method=singlediode_method,
+        )
+
+        # Set scale factor for current and voltage:
+        current_median = np.median(current)
+        voltage_median = np.median(voltage)
+
+
+        sdm = partial(pv_system_single_diode_model, **model_kwargs, **fixed_params)
+
+        residual = partial(self._pvpro_L2_loss,
+                        sdm=sdm,
+                        voltage=voltage,
+                        current=current,
+                        voltage_scale=voltage_median,
+                        current_scale=current_median,
+                        weights=weights,
+                        fit_params=fit_params)
+
+        if method == 'minimize':
+            if solver.lower() == 'nelder-mead':
+                bounds = None
+            elif solver.upper() == 'L-BFGS-B':
+                bounds = scipy.optimize.Bounds(
+                    [self._p_to_x(lower_bounds[k], k) for k in fit_params],
+                    [self._p_to_x(upper_bounds[k], k) for k in fit_params])
+            else:
+                raise Exception('solver must be "Nelder-Mead" or "L-BFGS-B"')
+
+            if verbose:
+                print('--')
+                print('p0:')
+                for k in fit_params:
+                    print(k, p0[k])
+                print('--')
+
+            if verbose:
+                print('Performing minimization... ')
+
+            if 'saturation_current_ref' in fit_params:
+                saturation_current_ref_start = p0['saturation_current_ref']
+                loss = np.inf
+                for Io_multiplier in saturation_current_multistart:
+
+                    p0[
+                        'saturation_current_ref'] = saturation_current_ref_start * Io_multiplier
+                    # Get numerical fit start point.
+                    x0 = [self._p_to_x(p0[k], k) for k in fit_params]
+
+                    res_curr = minimize(residual,
+                                        x0=x0,
+                                        bounds=bounds,
+                                        method=solver,
+                                        options=dict(
+                                            # maxiter=500,
+                                            disp=False,
+                                            # ftol=1e-9,
+                                        ),
+                                        )
+                    if verbose:
+                        print('Saturation current multiplier: ', Io_multiplier)
+                        print('loss:', res_curr['fun'])
+
+                    if res_curr['fun'] < loss:
+                        if verbose:
+                            print('New best value found, setting')
+                        res = res_curr
+                        loss = res_curr['fun']
+            else:
+                # Get numerical fit start point.
+                x0 = [self._p_to_x(p0[k], k) for k in fit_params]
+
+                res = minimize(residual,
+                            x0=x0,
+                            bounds=bounds,
+                            method=solver,
+                            options=dict(
+                                #    maxiter=100,
+                                disp=False,
+                                #    ftol=0.001,
+                            ),
+                            )
+
+            if verbose:
+                print(res)
+            n = 0
+            p_fit = {}
+            for param in fit_params:
+                p_fit[param] = self._x_to_p(res.x[n], param)
+                n = n + 1
+
+            out = {'p': p_fit,
+                'fixed_params': fixed_params,
+                'residual': res['fun'],
+                'x0': x0,
+                'p0': p0,
+                }
+            for k in res:
+                out[k] = res[k]
+
+            return out
 
     def estimate_p0(self,
                     verbose : bool =False,
