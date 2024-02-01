a0b66eb49909044ee5dccfdc7595d8f431807a01
lbj2011 <lbjxx@126.com>
2024-01-31 23:44:15 -0800

    version increase

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pvpro",
-    version="0.1.4",
+    version="0.1.5",
     author="baojieli",
     author_email="pvtools.lbl@gmail.com",
     description="Extract physical model paramaters from PV production data for degradation analysis and power prediction",
